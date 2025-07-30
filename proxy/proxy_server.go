8ab05fe98f9a13a0db75b796fc4c889901827520
PedroVhGit <pedrovh040110@gmail.com>
2025-07-30 12:19:22 -0400

    remove innecesary code

--- proxy/proxy_server.go
+++ proxy/proxy_server.go
@@ -8,14 +8,14 @@ import (
 	"github.com/networkgcorefullcode/scp/logger"
 )
 
-// This code creates a new HTTP server that listens on port 8080 for default and uses the `handleRequest`
+// This code creates a new HTTP server that listens on port 8080 for default
 // function to handle incoming requests. It also logs any errors
 // that occur while starting the server.
 func Start_Proxy_Server(httpPort int) {
-	// Create a new HTTP server with the handleRequest function as the handler
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
 	logger.AppLog.Infof("Starting proxy server on :%d", httpPort)
+
 	err := http.ListenAndServe(fmt.Sprintf(":%d", httpPort), proxy)
 	if err != nil {
 		logger.AppLog.Error("Error starting proxy server: ", err)
